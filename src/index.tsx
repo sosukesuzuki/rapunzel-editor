30d9fd90ece3a1fbc2e4eac5d313654783b6da1e
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-28 15:36:51 +0900

    Add normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
+import 'normalize.css'
 import App from './components/App'
 
 ReactDOM.render(<App />, document.querySelector('.root'))
