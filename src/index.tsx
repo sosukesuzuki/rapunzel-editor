24fd4993fda6728bf0da51524b37194ad354c882
Sosuke Suzuki <aosukeke@gmail.com>
2018-08-02 12:20:36 +0900

    Fix imports

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,14 @@ import { FileTreeStore } from './lib/stores/FileTreeStore'
 import { CurrentFileStore } from './lib/stores/CurrentFileStore'
 import { readFileNode } from './lib/utils/getFileTree'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFile } from '@fortawesome/free-solid-svg-icons'
-import { faFolder } from '@fortawesome/free-solid-svg-icons'
-import { faFolderOpen } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { faEdit } from '@fortawesome/free-solid-svg-icons'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
+import {
+  faFile,
+  faFolder,
+  faFolderOpen,
+  faTrash,
+  faEdit,
+  faEye
+} from '@fortawesome/free-solid-svg-icons'
 
 const g: any = global
 g.fs = fs
