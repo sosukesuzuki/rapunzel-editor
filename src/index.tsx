b3c422bf8aa2efc5b069d8a68bbc1c3f0af8af65
Sosuke Suzuki <aosukeke@gmail.com>
2018-08-12 17:53:50 +0900

    Create firstfile

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,9 @@ import {
   faCaretDown,
   faCaretRight
 } from '@fortawesome/free-solid-svg-icons'
+import { writeFile } from './lib/filesystem/commands/writeFile'
+import { readFile } from './lib/filesystem/queries/readFile'
+import { File } from './lib/types'
 
 const g: any = global
 g.fs = fs
@@ -37,8 +40,47 @@ library.add(
 )
 
 ;(async () => {
+  const hasVisited = !!localStorage.getItem('hasVisited')
+  localStorage.setItem('hasVisited', 'true')
+
+  let currentFileStore: CurrentFileStore
+  if (hasVisited) {
+    currentFileStore = new CurrentFileStore(null)
+  } else {
+    const readmePath = 'README.md'
+    await writeFile(readmePath, `
+# Rapunzel
+
+The fs based note-taking app focuses markdown.
+
+## How to develop?
+
+\`\`\`
+$ yarn
+$ yarn run dev
+\`\`\`
+
+## Author
+
+- [sosukesuzuki](https://github.com/sosukesuzuki)
+
+## Attention
+
+You can use Rapunzel on the modern browser only.
+
+## Licence
+
+MIT
+    `)
+    const content = await readFile(readmePath)
+    const readmeFile: File = {
+      pathname: readmePath,
+      content
+    }
+    currentFileStore = new CurrentFileStore(readmeFile)
+  }
+
   const fileTreeStore = new FileTreeStore(await readFileNode('.'))
-  const currentFileStore = new CurrentFileStore(null)
 
   ReactDOM.render(<App fileTreeStore={fileTreeStore} currentFileStore={currentFileStore} />, document.querySelector('.root'))
 })().catch(err => console.error(err))
