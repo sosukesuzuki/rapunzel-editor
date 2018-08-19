d580ccce9d6b0175f139220821527b1ebaff7c9f
Sosuke Suzuki <aosukeke@gmail.com>
2018-08-20 03:37:22 +0900

    Block Math! :fire:

--- src/ui/components/organisms/MarkdownRenderer.tsx
+++ src/ui/components/organisms/MarkdownRenderer.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import marked from 'marked'
+import katex from 'katex'
 import 'github-markdown-css'
+import 'katex/dist/katex.min.css'
 
 interface MarkdownRendererProps {
   content: string
@@ -9,12 +11,21 @@ interface MarkdownRendererProps {
 export default class MarkdownRenderer extends React.Component<MarkdownRendererProps> {
   static defaultProps: MarkdownRendererProps = { content: '' }
 
+  element: HTMLDivElement
+
   render () {
     const { content } = this.props
     const renderer = new marked.Renderer()
     renderer.link = (href, title, text) => (
       `<a target="_blank" rel="noopener noreferrer" href="${href}" title="${title}">${text}</a>`
     )
+    renderer.code = (code, lang, isEscaped) => {
+      if (lang === 'math') {
+        return `<object class="math">${katex.renderToString(code, { displayMode: true })}</math>`
+      } else {
+        return `<pre><code>${code}</code></pre>`
+      }
+    }
     const html = marked(content, { renderer })
 
     return (
