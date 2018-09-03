d6e46db88270f0f4968e5c73bd960a035c702ed2
Sosuke Suzuki <aosukeke@gmail.com>
2018-09-04 00:41:54 +0900

    Promisify localStorage function

--- specs/lib/localStorage/getSideNavWidth.spec.ts
+++ specs/lib/localStorage/getSideNavWidth.spec.ts
@@ -1,11 +1,25 @@
 import { getSideNavWidth } from '../../../src/lib/localStorage'
 
 describe('getSideNavWidth', () => {
+  afterEach(localStorage.clear)
+
   it('returns side nav from localStroage', async () => {
     // Given
     localStorage.setItem('sideNavWidth', (10).toString())
 
+    // When
+    const sideNavWidth = await getSideNavWidth()
+
     // Then
-    expect(getSideNavWidth()).toBe(10)
+    expect(sideNavWidth).toBe(10)
+  })
+
+  it('throws error when key is not found', async () => {
+    // When
+    const sideNavWidthPromise = getSideNavWidth()
+
+    // Then
+    expect(sideNavWidthPromise).rejects.toThrow()
+    expect(sideNavWidthPromise).rejects.toThrow('Key of sideNavWidth does not exists in localStorage')
   })
 })
--- src/lib/localStorage/getSideNavWidth.ts
+++ src/lib/localStorage/getSideNavWidth.ts
@@ -1,4 +1,7 @@
-export function getSideNavWidth () {
-  const sideNavWidth = parseInt(localStorage.getItem('sideNavWidth'), 10)
+import { getDataFromLocalStrage } from './getDataFromLocalStorage'
+
+export async function getSideNavWidth () {
+  const data = await getDataFromLocalStrage('sideNavWidth')
+  const sideNavWidth = parseInt(data, 10)
   return sideNavWidth
 }
