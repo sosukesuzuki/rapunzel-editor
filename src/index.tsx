8c7a170c3832fe414fbd23fa7dce32c0e4efe525
Sosuke Suzuki <aosukeke@gmail.com>
2018-09-08 00:17:51 +0900

    Use utils for currentfile in UI

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,8 @@ import { writeFile } from './lib/filesystem/commands'
 import { readFile } from './lib/filesystem/queries'
 import { File } from './lib/types'
 import { initializeIcons } from '@uifabric/icons'
-import { getHasVisited, setHasVisited } from './lib/localStorage'
+import { getHasVisited, setHasVisited, getCurrentFile } from './lib/localStorage'
+import READMEString from './lib/READMEString'
 
 const g: any = global
 g.fs = fs
@@ -24,36 +25,20 @@ initializeIcons('../assets/fonts/office-ui-fabric/')
 
   let currentFileStore: CurrentFileStore
   if (hasVisited) {
-    currentFileStore = new CurrentFileStore(null)
+    try {
+      const pathname = await getCurrentFile()
+      const content = await readFile(pathname)
+      const file = { pathname, content }
+      currentFileStore = new CurrentFileStore(file)
+    } catch (error) {
+      currentFileStore = new CurrentFileStore(null)
+    }
   } else {
-    const readmePath = 'README.md'
-    await writeFile(readmePath, `
-# Rapunzel
-
-The fs based note-taking app focuses markdown.
-
-## How to develop?
-
-\`\`\`
-$ yarn
-$ yarn run dev
-\`\`\`
-
-## Author
-
-- [sosukesuzuki](https://github.com/sosukesuzuki)
-
-## Attention
-
-You can use Rapunzel on the modern browser only.
-
-## Licence
-
-MIT
-    `)
-    const content = await readFile(readmePath)
+    let initialFilePath = 'README.md'
+    await writeFile(initialFilePath, READMEString)
+    const content = await readFile(initialFilePath)
     const readmeFile: File = {
-      pathname: readmePath,
+      pathname: initialFilePath,
       content
     }
     currentFileStore = new CurrentFileStore(readmeFile)
